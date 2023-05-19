ed66119e2616c59bb2b9d72418e3dbbf08774ba4
Dapper Foxtrot <60763515+theDapperFoxtrot@users.noreply.github.com>
2023-05-19 17:17:23 +0300

    starter styling

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,38 @@ import "./style/style.css";
 function App() {
 	return (
 		<>
-			<h1>Lloyd&apos;s Cafe & Bakery</h1>
+			{/* Video element used for looping background (simulating the effect of a gif) */}
+			<div className="background-video-container">
+				<video
+					id="background-video"
+					autoPlay
+					loop
+					muted
+				>
+					<source
+						src="../public/assets/img/lloyd-coffee.mp4"
+						type="video/mp4"
+					/>
+				</video>
+			</div>
+			<main>
+				<header>
+					<h1>Lloyd&apos;s Cafe & Bakery</h1>
+					<nav>
+						<ul>
+							<li>
+								<a href="menu.html">Menu</a>
+							</li>
+							<li>
+								<a href="index.html">Home</a>
+							</li>
+							<li>
+								<a href="about.html">About</a>
+							</li>
+						</ul>
+					</nav>
+				</header>
+			</main>
 		</>
 	);
 }
